0b070cc0b4af338a0568039bab78adb86c9a01c7
MadApach <30483715+MadApach@users.noreply.github.com>
2026-06-21 05:50:44 +0300

    Create ru 1_tune_btrfs_subvols.sh

---
+++ ru/1_tune_btrfs_subvols.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+echo "Создание сабволов для каталогов, не попадающих в снапшоты..."
+
+for dir in /var/cache /var/tmp; do
+echo
+echo "Обработка $dir"
+
+sudo mv "$dir" "${dir}.old"
+
+sudo btrfs subvolume create "$dir"
+
+sudo cp -a "${dir}.old"/. "$dir"/
+
+sudo rm -rf "${dir}.old"
+
+done
+
+echo
+echo "Готово."
+echo
+
+echo "Проверка:"
+sudo btrfs subvolume list /
